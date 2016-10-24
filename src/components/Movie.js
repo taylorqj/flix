d64d1c3bc6f0a5d20cfc07140100167240cce8fe
Taylor Jones <taylorqj@gmail.com>
2016-10-24 11:27:20 -0700

    Add some style to individual movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,27 +8,36 @@ export const Movie = ({ params: { movieId } }) => {
 
 	return (
 		<div>
-			<div className="movie-image">
-				<img src={movie.image} />
-			</div>
-
 			<div className="movie-title">
 				<h2>{ movie.name }</h2>
+
+				<hr />
 			</div>
 
-			<div className="movie-information">
-				<p>{ movie.director }</p>
-				<p>{ movie.released }</p>
+			<div className="movie-container">
+				<div className="movie-image">
+					<img src={movie.image} />
+				</div>
+
+				<div className="movie-information">
+					<p><b>Director:</b> { movie.director }</p>
+					<p><b>Release Date:</b> { movie.released }</p>
+				</div>
+
 			</div>
 
 			<div className="movie-reviews">
 				<h2>Reviews</h2>
 
+				<hr />
+
 				{movie.reviews.map((review, i) => (
-					<div key={i}>
-						<h3>{ review.title }</h3>
+					<div key={i} className="movie-review">
+						<h3>
+							{ review.title } <span className="review-author">by { review.author }</span>
+						</h3>
+
 						<p>{ review.body }</p>
-						<p>{ review.author }</p>
 					</div>
 				))}
 			</div>
