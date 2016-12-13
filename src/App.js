399299e15eb4952c4b5a88e3f489f2d188792940
Taylor Jones <taylorqj@gmail.com>
2016-12-13 09:21:34 -0800

    Add reload page token verification

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import './App.css';
 
 import Home from './components/Home';
@@ -11,31 +13,94 @@ import Router from 'react-router/BrowserRouter';
 import Match from 'react-router/Match';
 import Link from 'react-router/Link';
 import Miss from 'react-router/Miss';
-import MatchWhenAuthorized from './utils/auth';
+import MatchWhenAuthorized from './components/MatchWhenAuthorized';
 
-export const App = () => {
-    return (
-        <Router>
-            <div className="App">
-                <div className="App-header">
-                    <h2>Flix</h2>
+export class App extends Component {
+    static propTypes = {
+        authRequest: PropTypes.func.isRequired,
+        authRequestSuccess: PropTypes.func.isRequired,
+        authRequestFailure: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool.isRequired,
+    };
 
-                    <Link to="/">Home</Link>
-                    <Link to="/movies">Movies</Link>
+    componentWillMount() {
+        const {
+            authRequest,
+            authRequestSuccess,
+            authRequestFailure,
+        } = this.props;
+
+        const token = window.localStorage.getItem('token');
+
+        if (!token) {
+            return;
+        }
+
+        authRequest();
+
+        axios.post('http://localhost:3001/verify', token)
+            .then(response => {
+                authRequestSuccess();
+            })
+            .catch(e => (
+                authRequestFailure(e)
+            ));
+    }
+
+    render() {
+        const {
+            isAuthenticated,
+        } = this.props;
+
+        return (
+            <Router>
+                <div className="App">
+                    <div className="App-header">
+                        <h2>Flix</h2>
+
+                        <Link to="/">Home</Link>
+                        <Link to="/movies">Movies</Link>
+                    </div>
+
+                    <div className="container">
+                        <Match exactly pattern="/" component={Home} />
+                        <Match pattern="/login" component={Login} />
+
+                        {isAuthenticated && (
+                            <div>
+                                <MatchWhenAuthorized exactly pattern="/movies" component={Movies} />
+                                <MatchWhenAuthorized exactly pattern="/movies/:movieId" component={Movie} />
+                            </div>
+                            
+                        )}
+                    </div>
                 </div>
+            </Router>
+        );
+    }
+}
 
-                <div className="container">
-                    <Match exactly pattern="/" component={Home} />
-                    <Match pattern="/login" component={Login} />
+export function mapStateToProps({ auth: { isAuthenticated } }) {
+    return {
+        isAuthenticated,
+    };
+}
 
-                    <MatchWhenAuthorized pattern="/movies" component={Movies} />
-                    <MatchWhenAuthorized pattern="/movies/:movieId" component={Movie} />
+export function mapDispatchToProps(dispatch) {
+    return {
+        authRequest: () => dispatch({
+            type: 'AUTH_REQUEST',
+        }),
+        authRequestSuccess: () => dispatch({
+            type: 'AUTH_REQUEST_SUCCESS',
+        }),
+        authRequestFailure: () => dispatch({
+            type: 'AUTH_REQUEST_FAILURE',
+        }),
+    };
+}
 
-                    <Miss component={PageNotFound} />
-                </div>
-            </div>
-        </Router>
-    );
-};
-
-export default App;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(App);
