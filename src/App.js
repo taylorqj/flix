8d636195450b9115e8f55b8bdfa13083afa304df
Taylor Jones <taylorqj@gmail.com>
2016-12-15 09:45:11 -0800

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
 
 import Home from './components/Home';
@@ -15,92 +13,27 @@ import Link from 'react-router/Link';
 import Miss from 'react-router/Miss';
 import MatchWhenAuthorized from './components/MatchWhenAuthorized';
 
-export class App extends Component {
-    static propTypes = {
-        authRequest: PropTypes.func.isRequired,
-        authRequestSuccess: PropTypes.func.isRequired,
-        authRequestFailure: PropTypes.func.isRequired,
-        isAuthenticated: PropTypes.bool.isRequired,
-    };
+export const App = () => {
+    return (
+        <Router>
+            <div className="App">
+                <div className="App-header">
+                    <h2>Flix</h2>
 
-    componentWillMount() {
-        const {
-            authRequest,
-            authRequestSuccess,
-            authRequestFailure,
-        } = this.props;
+                    <Link to="/">Home</Link>
+                    <Link to="/movies">Movies</Link>
+              </div>
 
-        const token = window.localStorage.getItem('token');
+              <div className="container">
+                  <Match exactly pattern="/" component={Home} />
+                  <Match pattern="/login" component={Login} />
 
-        if (!token) {
-            return;
-        }
+                  <MatchWhenAuthorized exactly pattern="/movies" component={Movies} />
+                  <MatchWhenAuthorized pattern="/movies/:movieId" component={Movie} />
+              </div>
+          </div>
+      </Router>
+  );
+};
 
-        authRequest();
-
-        axios.post('http://localhost:3001/verify', token)
-            .then(response => {
-                authRequestSuccess();
-            })
-            .catch(e => (
-                authRequestFailure(e)
-            ));
-    }
-
-    render() {
-        const {
-            isAuthenticated,
-        } = this.props;
-
-        return (
-            <Router>
-                <div className="App">
-                    <div className="App-header">
-                        <h2>Flix</h2>
-
-                        <Link to="/">Home</Link>
-                        <Link to="/movies">Movies</Link>
-                    </div>
-
-                    <div className="container">
-                        <Match exactly pattern="/" component={Home} />
-                        <Match pattern="/login" component={Login} />
-
-                        {isAuthenticated && (
-                            <div>
-                                <MatchWhenAuthorized exactly pattern="/movies" component={Movies} />
-                                <MatchWhenAuthorized exactly pattern="/movies/:movieId" component={Movie} />
-                            </div>
-                            
-                        )}
-                    </div>
-                </div>
-            </Router>
-        );
-    }
-}
-
-export function mapStateToProps({ auth: { isAuthenticated } }) {
-    return {
-        isAuthenticated,
-    };
-}
-
-export function mapDispatchToProps(dispatch) {
-    return {
-        authRequest: () => dispatch({
-            type: 'AUTH_REQUEST',
-        }),
-        authRequestSuccess: () => dispatch({
-            type: 'AUTH_REQUEST_SUCCESS',
-        }),
-        authRequestFailure: () => dispatch({
-            type: 'AUTH_REQUEST_FAILURE',
-        }),
-    };
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(App);
+export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,115 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import Redirect from 'react-router/Redirect';
+import jwtDecode from 'jwt-decode';
 
-export const Login = () => (
-    <div>
-        Login
-    </div>
-);
+export class Login extends Component {
+    static propTypes = {
+        authRequest: PropTypes.func.isRequired,
+        authRequestSuccess: PropTypes.func.isRequired,
+        authRequestFailure: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool.isRequired,
+    };
 
-export default Login;
+    state = {
+        email: '',
+        password: '',
+    };
+
+    handleUsernameChange = event => {
+        this.setState({ email: event.target.value });
+
+        event.preventDefault();
+    }
+
+    handlePasswordChange = event => {
+        this.setState({ password: event.target.value });
+
+        event.preventDefault();
+    }
+
+    handleSubmit = event => {
+        const {
+            authRequest,
+            authRequestSuccess,
+            authRequestFailure,
+        } = this.props;
+
+        authRequest();
+
+        axios.post('http://localhost:3001/login', {
+            email: this.state.email,
+            password: this.state.password,
+        })
+            .then(res => {
+                authRequestSuccess(res.data.token);
+            })
+            .catch(e => {
+                authRequestFailure(e);
+            })
+
+        event.preventDefault();
+    }
+
+    render() {
+        const {
+            isAuthenticated,
+        } = this.props;
+
+        if (isAuthenticated) {
+            return <Redirect to={{ pathname: '/' }} />
+        }
+
+        return (
+            <div>
+                Login
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>Email</label>
+                    <input type="text" value={this.state.email} onChange={this.handleUsernameChange} />
+
+                    <label>Password</label>
+                    <input type="text" value={this.state.password} onChange={this.handlePasswordChange} />
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export function mapStateToProps({ auth: { isAuthenticated } }) {
+    return { isAuthenticated };
+}
+
+export function mapDispatchToProps(dispatch) {
+    return {
+        authRequest: data => dispatch({ type: 'AUTH_REQUEST' }),
+
+        authRequestSuccess: data => {
+            window.localStorage.setItem('token', data);
+
+            const decoded = jwtDecode(data);
+
+            dispatch({
+                type: 'AUTH_REQUEST_SUCCESS',
+                payload: decoded,
+            });
+        },
+
+        authRequestFailure: data => {
+            window.localStorage.removeItem('token');
+
+            dispatch({
+                type: 'AUTH_REQUEST_FAILURE',
+                payload: data,
+            });
+        },
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Login);
